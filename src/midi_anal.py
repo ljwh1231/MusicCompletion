38c7c4767bce5b14df37392f6bd0c94f2adb6832
SeoNamgyu <zych1751@naver.com>
2019-04-11 23:56:48 +0900

    image to roll/ roll to midi

--- src/midi_anal.py
+++ src/midi_anal.py
@@ -5,10 +5,9 @@ import matplotlib as mpl
 from matplotlib.colors import colorConverter
 import midi
 
+
 class MidiFile(mido.MidiFile):
-
     def __init__(self, filename):
-
         mido.MidiFile.__init__(self, filename)
         self.sr = 10
         self.meta = {}
@@ -30,12 +29,12 @@ class MidiFile(mido.MidiFile):
                     events[channel].append(msg)
                 except AttributeError:
                     try:
-                        if type(msg) != type(mido.UnknownMetaMessage):
+                        if not isinstance(msg, mido.UnknownMetaMessage):
                             self.meta[msg.type] = msg.dict()
                         else:
                             pass
                     except:
-                        print("error",type(msg))
+                        print("error", type(msg))
 
         return events
 
@@ -46,6 +45,7 @@ class MidiFile(mido.MidiFile):
         sr = self.sr
         # compute total length in tick unit
         length = self.get_total_ticks()
+
         # allocate memory to numpy array
         roll = np.zeros((16, 128, length // sr), dtype="int8")
         # print(roll.shape)
@@ -55,7 +55,6 @@ class MidiFile(mido.MidiFile):
 
         # use a register array to save the state(program_change) for each channel
         timbre_register = [1 for x in range(16)]
-
 
         for idx, channel in enumerate(events):
             time_counter = 0
@@ -78,28 +77,23 @@ class MidiFile(mido.MidiFile):
                     timbre_register[idx] = msg.program
                     # print("channel", idx, "pc", msg.program, "time", time_counter, "duration", msg.time)
 
-
-
                 if msg.type == "note_on":
                     # print("on ", msg.note, "time", time_counter, "duration", msg.time, "velocity", msg.velocity)
                     note_on_start_time = time_counter // sr
                     note_on_end_time = (time_counter + msg.time) // sr
                     intensity = volume * msg.velocity // 127
 
-
-
-					# When a note_on event *ends* the note start to be play
-					# Record end time of note_on event if there is no value in register
-					# When note_off event happens, we fill in the color
+                    # When a note_on event *ends* the note start to be play
+                    # Record end time of note_on event if there is no value in register
+                    # When note_off event happens, we fill in the color
                     if note_register[msg.note] == -1:
                         note_register[msg.note] = (note_on_end_time,intensity)
                     else:
-					# When note_on event happens again, we also fill in the color
+                        # When note_on event happens again, we also fill in the color
                         old_end_time = note_register[msg.note][0]
                         old_intensity = note_register[msg.note][1]
                         roll[idx, msg.note, old_end_time: note_on_end_time] = old_intensity
                         note_register[msg.note] = (note_on_end_time,intensity)
-
 
                 if msg.type == "note_off":
                     # print("off", msg.note, "time", time_counter, "duration", msg.time, "velocity", msg.velocity)
@@ -107,7 +101,7 @@ class MidiFile(mido.MidiFile):
                     note_off_end_time = (time_counter + msg.time) // sr
                     note_on_end_time = note_register[msg.note][0]
                     intensity = note_register[msg.note][1]
-					# fill in color
+                    # fill in color
                     roll[idx, msg.note, note_on_end_time:note_off_end_time] = intensity
 
                     note_register[msg.note] = -1  # reinitialize register
@@ -179,11 +173,23 @@ class MidiFile(mido.MidiFile):
         return max_ticks
 
     def draw_sessions(self, roll):
-        print(len(roll))
         for track_num in range(len(roll)):
             track_img = roll[track_num]
             try:
-                plt.imsave('result_images/' + 'track' + str(track_num) + '.png', track_img)
+                plt.imsave('result_images/' + 'track' + str(track_num) + '.png', track_img, cmap=plt.get_cmap('gray'))
+                if track_num == 0:
+                    img = plt.imread('result_images/' + 'track' + str(track_num) + '.png')
+                    max_val = -1
+                    min_val = 128
+                    for line in track_img:
+                        for val in line:
+                            max_val = max(max_val, val)
+                            min_val = min(min_val, val)
+                    print(max_val, min_val)
+                    # print('original ----------------------')
+                    # print(track_img.shape)
+                    # print(track_img)
+                    image_to_roll(img, min_val, max_val)
                 print("image save complete")
             except:
                 print("can't save image")
@@ -200,13 +206,96 @@ def extract_track(file):
     #         4 = drum -> track9
     #         5 = melody -> track13
     for idx, it in enumerate(pattern):
-        if idx == 1 :
+        if idx == 1:
             extracted.append(it)
 
     midi.write_midifile("./result_midi/result_DLBIA.mid", extracted)
+
+
+def image_to_roll(img, min_val, max_val):
+    ret_img = np.array([[int(img[i][j][0] * (max_val - min_val) + min_val) for j in range(img.shape[1])]
+                        for i in range(img.shape[0])])
+    plt.imsave('result_images/temp.png', ret_img, cmap=plt.get_cmap('gray'))
+    return ret_img
+
+
+def roll_to_midi(rolls, tick_per_time_slice, resolution=120):
+    # melody is index 0
+    # guitar is index 1
+    # piano is index 2
+    # bass is index 3
+    # drum is index 4
+
+    result = midi.Pattern(resolution=resolution*100)
+    for i in range(5):
+        if i >= len(rolls):
+            continue
+
+        roll = rolls[i]
+        channel = i
+
+        # for drum
+        if channel == 4:
+            channel = 9
+
+        track = midi.Track()
+        if i == 0:
+            track.append(midi.ProgramChangeEvent(tick=0, data=[53], channel=channel))
+        elif i == 1:
+            track.append(midi.ProgramChangeEvent(tick=0, data=[29], channel=channel))
+        elif i == 2:
+            track.append(midi.ProgramChangeEvent(tick=0, data=[0], channel=channel))
+        elif i == 3:
+            track.append(midi.ProgramChangeEvent(tick=0, data=[34], channel=channel))
+        else:
+            track.append(midi.ProgramChangeEvent(tick=0, data=[1], channel=channel))
+
+        current_tick = 0
+        prev_intensity = [0 for _ in range(128)]
+        prev_tick = 0
+
+        for time_slice in range(roll.shape[1]):
+            for pitch in range(roll.shape[0]):
+                if roll[pitch][time_slice] != prev_intensity[pitch]:
+                    diff_tick = current_tick - prev_tick
+                    if roll[pitch][time_slice] == 0:
+                        track.append(midi.NoteOffEvent(tick=diff_tick,
+                                                       data=[pitch, prev_intensity[pitch]],
+                                                       channel=channel))
+                        prev_intensity[pitch] = 0
+                        prev_tick = current_tick
+                    else:
+                        if prev_intensity[pitch] == 0:
+                            track.append(midi.NoteOnEvent(tick=diff_tick,
+                                                          data=[pitch, roll[pitch][time_slice]],
+                                                          channel=channel))
+                            prev_intensity[pitch] = roll[pitch][time_slice]
+                            prev_tick = current_tick
+                        else:
+                            track.append(midi.NoteOffEvent(tick=diff_tick,
+                                                           data=[pitch, prev_intensity[pitch]],
+                                                           channel=channel))
+                            track.append(midi.NoteOnEvent(tick=diff_tick,
+                                                          data=[pitch, roll[pitch][time_slice]],
+                                                          channel=channel))
+                            prev_intensity[pitch] = roll[pitch][time_slice]
+                            prev_tick = current_tick
+                current_tick += tick_per_time_slice
+
+        track.append(midi.EndOfTrackEvent(tick=0, channel=channel))
+        # if i == 1:
+        result.append(track)
+    return result
+
+
+# DLBIA : 1 = piano -> track0
+#         2 = bass -> track1
+#         3 = guitar -> track7
+#         4 = drum -> track9
+#         5 = melody -> track13
 if __name__ == "__main__":
     mid = MidiFile("./midi_files/DLBIA.mid")
-    track_ext = extract_track("./midi_files/DLBIA.mid")
+    extract_track("./midi_files/DLBIA.mid")
     # mid = MidiFile("./result_midi/result_DLBIA.mid")
     # track_ext = extract_track("./result_midi/result_DLBIA.mid")
     # get the list of all events
@@ -214,7 +303,9 @@ if __name__ == "__main__":
 
     # get the np array of piano roll image
     roll = mid.get_roll()
-    mid.draw_sessions(roll)
+    ret_midi = roll_to_midi([roll[13], roll[7], roll[0], roll[1], roll[9]], 10, 240)
+    midi.write_midifile('result.mid', ret_midi)
+    # mid.draw_sessions(roll)
     # draw piano roll by pyplot
     # mid.draw_roll()
 
